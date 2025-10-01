26e8abaaafc7e8a679e6402933d3d2a9e953dab7
Aarav Bhatt <aaravbhatt76223@gmail.com>
2025-10-01 15:43:49 -0400

    Started Firebase implementation, added way to push .json data of games to Firestore.

---
+++ scorigamis/scripts/firestore_upload.py
@@ -0,0 +1,24 @@
+import firebase_admin
+from firebase_admin import firestore, credentials
+import json
+import os
+
+script_dir = os.path.dirname(__file__)
+key_path = os.path.join(script_dir, "serviceAccountKey.json");
+
+cred = credentials.Certificate(key_path)
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+json_path = os.path.join(script_dir, "..", "public", "nfl_scores_cleaned.json") 
+
+with open(json_path, 'r') as d:
+    data = json.load(d);
+
+games_collection = db.collection("games")
+
+for game in data:
+    games_collection.add(game)
+
+print("Upload to Firestore complete.")
